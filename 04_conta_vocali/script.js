9857567f2a16e00bd2fff092059ab23cb7fa16ec
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-18 01:28:35 +0100

    add condition inside the function + invoke the function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,11 +5,23 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 function vowelsCounter(str) {
-    
+
+    const vowels ='aeiouAEIOU';
+    const count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+          count++;
+        }
+        return count
+}
+
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const checkVowels = vowelsCounter(word);
+console.log(checkVowels);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
