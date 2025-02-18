3324af6c112c3e2af40abdb7ffdec45824825109
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-18 01:19:33 +0100

    fix function snack 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,19 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 function firstLetterFilter(words, letters) {
-    let wordsFiltrate = [];
-    
+    let wordFiltrate = [];
+  
     for (let i = 0; i < words.length; i++) {
-        if(words[i].toLowerCase() === letters.toLoweCase()){
-            wordsFiltrate.push(words[i]);
-        }
-        return wordsFiltrate;
+      if (words[i].charAt(0).toLowerCase() === letters.toLowerCase()) {
+        wordFiltrate.push(words[i]);
+      }
     }
+    
+    return wordFiltrate;
+  }
 
-}
 // Invoca la funzione qui e stampa il risultato in console
 let firstLetter = 'A'
 
 const filter = firstLetterFilter(names, firstLetter);
+console.log(filter);
+
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
